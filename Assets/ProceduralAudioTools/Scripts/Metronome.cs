5b307b3085fe61f76c30695439cc3637c37d574a
bryanrtboy <bryan@bryanleister.com>
2016-12-28 18:58:15 -0700

    Initial Committ
    A procedural audio project, using very simple metronome and objects to
    contain multiple AudioSources.

---
+++ Assets/ProceduralAudioTools/Scripts/Metronome.cs
@@ -0,0 +1,33 @@
+﻿//Bryan Leister - Jan 2017
+using System.Collections;
+using UnityEngine;
+
+// The code example shows how to implement a metronome that procedurally generates the click sounds via the OnAudioFilterRead callback.
+// While the game is paused or the suspended, this time will not be updated and sounds playing will be paused. Therefore developers of
+//  music scheduling routines do not have to do any rescheduling after the app is unpaused
+
+namespace ProceduralAudioTools
+{
+	public class Metronome : MonoBehaviour
+	{
+		public static Metronome instance = null;
+	
+		public float bpm = 140.0F;
+
+		void Awake ()
+		{
+			//Check if instance already exists
+			if (instance == null) {
+				//if not, set instance to this
+//				Debug.Log ("Instance is null at " + Time.time);
+				instance = this;
+				//If instance already exists and it's not this:
+			} else if (instance != this) {
+				Debug.Log ("Instance does not equal this at " + Time.time);
+				//Then destroy this. This enforces our singleton pattern, meaning there can only ever be one instance of a TouchDetector.
+				Destroy (gameObject); 
+			}
+		}
+
+	}
+}
---
+++ Assets/ProceduralAudioTools/Scripts/MetronomeListener.cs
@@ -0,0 +1,105 @@
+﻿//Bryan Leister - Jan 2017
+
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Audio;
+
+namespace ProceduralAudioTools
+{
+	public class MetronomeListener : MonoBehaviour
+	{
+
+		public AudioClip[] m_clips;
+		public bool m_randomize = true;
+		public float m_clipsSpatialBlend = 0f;
+		public float m_audioLevel = 1;
+		public AudioMixerGroup m_audioMixer;
+		public int numBeatsPerSegment = 16;
+		public float m_randomChanceOfPlaying = 1f;
+		public bool m_pingPan = false;
+		//This only works with slower sounds, i.e. the setScheduled is going to play the sound at a specific time, which may not correspond with the pan settings...
+
+		private double nextEventTime;
+		private int randClip = 0;
+		private int lastRandClip = 0;
+		private AudioSource[] audioSources;
+		private bool running = false;
+		private int count;
+		private Metronome m;
+		private bool isLeft = false;
+
+		void Awake ()
+		{
+			m = FindObjectOfType<Metronome> () as Metronome;
+			if (m_clips.Length < 2) {
+				Debug.LogError ("Not enough clips to play, must have more than 2 clips!");
+				Destroy (this.gameObject);
+			}
+		}
+
+		void Start ()
+		{
+			int length = m_clips.Length;
+			audioSources = new AudioSource[length];
+
+			int i = 0;
+			while (i < length) {
+				GameObject child = new GameObject (this.name + " Audio Player " + i.ToString ());
+
+				child.transform.SetParent (this.transform);
+				audioSources [i] = child.AddComponent<AudioSource> ();
+				audioSources [i].spatialBlend = m_clipsSpatialBlend;
+				audioSources [i].volume = m_audioLevel;
+				if (m_audioMixer != null)
+					audioSources [i].outputAudioMixerGroup = m_audioMixer;
+
+				i++;
+			}
+			nextEventTime = AudioSettings.dspTime + 2.0F;
+			running = true;
+		}
+
+		void Update ()
+		{
+			if (!running)
+				return;
+
+			if (m == null)
+				m = FindObjectOfType<Metronome> () as Metronome;
+        
+			double time = AudioSettings.dspTime;
+
+			if (time + 1.0F > nextEventTime) {
+				if (m_randomize) {
+					while (randClip == lastRandClip) {											
+						randClip = Random.Range (0, m_clips.Length);
+						continue;
+					}
+					lastRandClip = randClip;
+				} else {
+					randClip = count;
+				}
+
+				audioSources [count].clip = m_clips [randClip];
+
+				if (m_pingPan) {
+
+					if (isLeft)
+						audioSources [randClip].panStereo = -1f;
+					else
+						audioSources [randClip].panStereo = 1f;
+
+					isLeft = !isLeft;
+				}
+
+				if (Random.Range (0f, 1f) < m_randomChanceOfPlaying)
+					audioSources [count].PlayScheduled (nextEventTime);
+				//Debug.Log ("Scheduled source " + randClip + " to start at time " + nextEventTime);
+				nextEventTime += 60.0F / m.bpm * numBeatsPerSegment; //calculate the next nextEventTime....
+				count++;
+				if (count >= m_clips.Length)
+					count = 0;
+			}
+		}
+	}
+}
